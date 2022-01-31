8cee17219197b615cde686ffbf8e3252bbbbb337
Christopher Chalcraft <chris.chalcraft@gmail.com>
2022-01-30 22:03:31 -0800

    resetting poetry

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nix-poetry
 Experimentation on building python apps with poetry2nix
+
+- [useful poetry2nix snippets](https://pythonrepo.com/repo/nix-community-poetry2nix)
--- shell.nix
+++ shell.nix
@@ -1,19 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
-
-with import <nixpkgs> {};
-let pythonEnv = python38.withPackages (ps: with ps; [
-  flake8
-  black
-  pynvim
-  ipython
-  python-language-server.override { pylint = null; }
-  pip
-  poetry
-  ]);
-
-in
-pkgs.mkShell  {
-  buildInputs = with pkgs; [
-    pythonEnv
-  ];
+pkgs.mkShell {
+  buildInputs = [ pkgs.python38 pkgs.poetry ];
 }
+
